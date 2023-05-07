a723739511c940f370de22e0bf1b79f9c895a4ab
BambiMC <fabians.viper@arcor.de>
2023-05-07 22:33:01 +0200

    changing structure of ebony tree

--- src/main/java/net/bambi/orefusion/worldgen/ModConfiguredFeatures.java
+++ src/main/java/net/bambi/orefusion/worldgen/ModConfiguredFeatures.java
@@ -8,14 +8,18 @@ import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.valueproviders.ConstantInt;
+import net.minecraft.util.valueproviders.IntProvider;
+import net.minecraft.util.valueproviders.UniformInt;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.OreConfiguration;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
 import net.minecraft.world.level.levelgen.feature.featuresize.TwoLayersFeatureSize;
+import net.minecraft.world.level.levelgen.feature.foliageplacers.AcaciaFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.foliageplacers.BlobFoliagePlacer;
 import net.minecraft.world.level.levelgen.feature.stateproviders.BlockStateProvider;
+import net.minecraft.world.level.levelgen.feature.trunkplacers.ForkingTrunkPlacer;
 import net.minecraft.world.level.levelgen.feature.trunkplacers.StraightTrunkPlacer;
 import net.minecraft.world.level.levelgen.structure.templatesystem.RuleTest;
 import net.minecraft.world.level.levelgen.structure.templatesystem.TagMatchTest;
@@ -40,9 +44,10 @@ public class ModConfiguredFeatures {
                 OreConfiguration.target(stoneReplaceables, ModBlocks.LIMESTONE.get().defaultBlockState()));
 
         register(context, EBONY_KEY, Feature.TREE,
-                new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(ModBlocks.EBONY_LOG.get()), new StraightTrunkPlacer(5, 6, 3),
-                        BlockStateProvider.simple(ModBlocks.EBONY_LEAVES.get()), new BlobFoliagePlacer(ConstantInt.of(2), ConstantInt.of(0), 4),
-                        new TwoLayersFeatureSize(1, 0, 2)).build());
+                new TreeConfiguration.TreeConfigurationBuilder(BlockStateProvider.simple(ModBlocks.EBONY_LOG.get()), new ForkingTrunkPlacer(6, 1, 3),
+                        BlockStateProvider.simple(ModBlocks.EBONY_LEAVES.get()), new AcaciaFoliagePlacer(UniformInt.of(1, 3),
+                                UniformInt.of(0, 0)),
+                        new TwoLayersFeatureSize(5, 8, 2)).build());
 
         //veins size max 9 / 64, MAX IS 64!
         register(context, OVERWORLD_PHOSPHATE_ORE_KEY, Feature.ORE, new OreConfiguration(overworldPhosphateOres, 9));
